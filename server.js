5e1173ab72adcb7677ad1a0cd46b33d94a240900
Jeong-Rae <kkwjdfo@gmail.com>
2024-07-31 14:27:13 +0900

    feat: console 추가

--- server.js
+++ server.js
@@ -3,23 +3,33 @@ import "./env.config.js";
 import express from "express";
 import { createServer } from "http";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import voiceRouter from "./routes/voice.router.js";
 import fileRouter from "./routes/file.router.js";
 import chatbotRouter from "./routes/chatbot.routes.js";
 import infoRouter from "./routes/info.routes.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const server = createServer(app);
 
 const corsConfig = {
-    origin: "http://localhost:4137",
+    origin: ["http://localhost:4137", "http://localhost:4000"],
     credentials: true,
 };
 
 app.use(cors(corsConfig));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.use(voiceRouter);
 app.use(fileRouter);
